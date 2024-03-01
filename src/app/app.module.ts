8227f2c38e803d2190b96a35fa18abd55aa32f16
Webtree-design <info@webtree-design.de>
2024-03-01 08:48:53 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistartionComponent } from './components/registartion/registartion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistartionComponent
   ],
   imports: [
     BrowserModule,
